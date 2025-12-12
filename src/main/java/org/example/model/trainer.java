373e8c3ff35a8dfa8cf296abe1d2dc3d6ee4feed
s7zs <hussinayman766@gmail.com>
2025-12-13 01:46:56 +0200

    2

--- src/main/java/org/example/model/trainer.java
+++ src/main/java/org/example/model/trainer.java
@@ -2,7 +2,7 @@ package org.example.model;
 
 public class trainer extends users{
 
-    public trainer(Roles role, String password, String username) {
+    public trainer(Roles Roles, String password, String username) {
         super(Roles.trainer, password, username);
     }
 }
